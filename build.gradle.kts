9bb7148f8a339372923cbc032c21dd9dc8544958
grimsa <gediminas.rimsa@gmail.com>
2020-11-14 11:41:18 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    java
+}
+
+group = "com.github.grimsa.loggingpermits"
+version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_15
+    targetCompatibility = JavaVersion.VERSION_15
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
