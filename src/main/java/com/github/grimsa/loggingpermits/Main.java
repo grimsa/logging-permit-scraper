9a744397ac9920ad8c1547c5e00eb8225c73044d
grimsa <gediminas.rimsa@gmail.com>
2020-11-18 01:10:00 +0200

    Change to output locations

--- src/main/java/com/github/grimsa/loggingpermits/Main.java
+++ src/main/java/com/github/grimsa/loggingpermits/Main.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -29,16 +29,28 @@ public class Main {
                 .flatMap(List::stream)
                 .collect(Collectors.toList());
         log.info("Retrieved results: " + allPermits.size());
-        writeToFile(allPermits, "results-" + Instant.now().getEpochSecond() + ".csv");
+        writePermitsToFile("data/leidimai.csv", allPermits);
+        writeReadmeToFile("data/README.md", allPermits.size());
     }
 
-    private static void writeToFile(List<LoggingPermit> allPermits, String fileName) throws IOException {
-        FileWriter out = new FileWriter(fileName);
-        CSVFormat csvFormat = CSVFormat.DEFAULT.withHeader(allPermits.get(0).columnNames().toArray(String[]::new));
-        try (CSVPrinter printer = new CSVPrinter(out, csvFormat)) {
-            Failable.stream(allPermits.stream())
-                    .map(LoggingPermit::columnValues)
-                    .forEach(printer::printRecord);
+    private static void writePermitsToFile(String fileName, List<LoggingPermit> allPermits) throws IOException {
+        try (FileWriter out = new FileWriter(fileName, StandardCharsets.UTF_8, false)) {
+            CSVFormat csvFormat = CSVFormat.DEFAULT.withHeader(allPermits.get(0).columnNames().toArray(String[]::new));
+            try (CSVPrinter printer = new CSVPrinter(out, csvFormat)) {
+                Failable.stream(allPermits.stream())
+                        .map(LoggingPermit::columnValues)
+                        .forEach(printer::printRecord);
+            }
+        }
+    }
+
+    private static void writeReadmeToFile(String fileName, int numberOfPermits) throws IOException {
+        try (FileWriter out = new FileWriter(fileName, StandardCharsets.UTF_8, false)) {
+            out.write("""
+                    Duomenys atnaujinti: %s
+                    Leidimų skaičius: %s
+                    """.formatted(Instant.now().toString(), numberOfPermits)
+            );
         }
     }
 }
